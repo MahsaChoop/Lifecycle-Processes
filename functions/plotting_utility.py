56e0ab87e0d0897a3c1943cb8d195b48c79fb482
choop <mchoopannejad@gmail.com>
2026-08-10 14:25:46 +0200

    adding split miner to f1 comparsion figure

--- functions/plotting_utility.py
+++ functions/plotting_utility.py
@@ -314,7 +314,7 @@ def plot_f_score_dotplot_by_miner_different_repos(
     f_score_comparison,
     repos=None,
     discovery_methods=None,
-    ncols=4,
+    ncols=3,
     figures_dir=None,
     stem="f_score_dotplot_by_miner",
     show=True,
